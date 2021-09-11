badba1815b1ff827fbf66d91bfc04a1a640ef9ae
Ranganath-MD <>
2021-09-11 22:32:58 +0530

    changes in document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,31 +8,6 @@ import Document, {
 } from "next/document";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
 
   render() {
     return (
@@ -42,6 +17,7 @@ export default class MyDocument extends Document {
             rel="icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💻</text></svg>"
           />
+          <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="" />
           <link
             href="https://fonts.googleapis.com/css2?family=Arvo:wght@400;700&display=swap"
             rel="stylesheet"
